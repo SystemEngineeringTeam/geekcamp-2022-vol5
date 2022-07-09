a79b7b8421f8aebb9a17479bb4fb40e759798bdf
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-07-09 16:08:48 +0900

    twiiter

--- hiding-twitter/src/python/twitter.py
+++ hiding-twitter/src/python/twitter.py
@@ -2,10 +2,11 @@ import tweepy
 import json
 import os
 from dotenv import load_dotenv
-from datetime import timedelta 
+from datetime import timedelta
 # 先ほど取得した各種キーを代入する
 load_dotenv()
 
+# 各種キーを.envからセット
 CK = os.environ['CK']
 CS = os.environ['CS']
 AT = os.environ['AT']
@@ -17,26 +18,29 @@ auth.set_access_token(AT, AS)
 
 api = tweepy.API(auth)
 
+# 投稿時刻のフォーマット修正
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 list = []
-
+i = 1
 #1ツイートずつループ
 for status in api.home_timeline():
+    #デバッグ用
     #見映えのため区切る
-    print('-------------------------------------------')
-    #ユーザ名表示
-    print('name:' + status.user.name)
-    #内容表示
-    print(status.id)
-    print(status.text)
-    print(status.created_at + timedelta(hours=9))
-    print(status.favorite_count)
-    print(status.retweet_count)
+    # print('-------------------------------------------')
+    # #ユーザ名表示
+    # print('name:' + status.user.name)
+    # #内容表示
+    # print(status.id)
+    # print(status.text)
+    # print(status.created_at + timedelta(hours=9))
+    # print(status.favorite_count)
+    # print(status.retweet_count)
     #json形式で出力するための準備
     str = {
-        status.user.name:{
+        i:{
+            "name":status.user.name,
             "text":status.text,
             "id":status.id,
             "time":date_handler(status.created_at + timedelta(hours=9)),
@@ -46,6 +50,7 @@ for status in api.home_timeline():
     }
     #listに追加する
     list.append(str)
+    i += 1
     #JSON形式で出力
 with open('./twitter.json', 'w') as f:
     json.dump(list, f, ensure_ascii=False, indent=4)
